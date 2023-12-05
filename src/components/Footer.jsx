9e9ce45150d072932e7127789701ffa775501ff8
dimaliwatkent <dimaliwatkent@gmail.com>
2023-12-05 10:08:17 +0800

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex text-sm m-2 text-base justify-center items-center">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="pl-1"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a href="#" className="px-1">
+        dimaliwatkent
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
